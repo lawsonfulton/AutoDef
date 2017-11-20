819dfa150d1f68239372be9cdd10b1e3148715c2
zero-impact <lawsonfulton@gmail.com>
2017-11-20 15:27:52 -0500

    pca working

--- learn.py
+++ learn.py
@@ -9,10 +9,29 @@ from iglhelpers import e2p, p2e
 
 import my_utils
 
+# Global Vars
 current_frame = 0
-
-
+show_decoded = False
 base_path = 'training_data/first_interaction/'
+
+def pca_analysis(data, n_components):
+    print("Doing PCA...")
+    # numpy.random.shuffle(numpy_displacements_sample) # Necessary?
+    pca = PCA(n_components=n_components)
+
+    n_samples = len(data)
+    sample_dim = len(data[0])
+    point_dim = len(data[0][0])
+
+    flattened_data = data.reshape((n_samples, sample_dim * point_dim))
+    pca.fit(flattened_data)
+
+    pca_encoded = pca.transform(flattened_data)
+    pca_decoded = pca.inverse_transform(pca_encoded).reshape((n_samples, sample_dim, point_dim))
+    
+    return pca_decoded, pca_encoded
+
+
 def main():
     # Loading the rest pose
     base_verts, face_indices = my_utils.load_base_vert_and_face_dmat_to_numpy(base_path)
@@ -20,29 +39,30 @@ def main():
     face_indices_eig = p2e(face_indices)
 
     # Loading displacements for training data
-    displacements_data = my_utils.load_displacement_dmats_to_numpy(base_path)
+    displacements = my_utils.load_displacement_dmats_to_numpy(base_path)
 
 
-    
+    # Do the PCA analysis
+    n_components = 3
+    decoded_pca_displacements, encoded_pca_displacements = pca_analysis(displacements, n_components)
+
     # Set up
     viewer = igl.viewer.Viewer()
     viewer.data.set_mesh(base_verts_eig, face_indices_eig)
 
 
     def pre_draw(viewer):
-        global current_frame
+        global current_frame, show_decoded
         
         if viewer.core.is_animating:
-            viewer.data.set_vertices(p2e(base_verts + displacements_data[current_frame]))
-            # if show_decoded:
-            #     viewer.data.set_vertices(test_data_decoded_eigen[current_frame])
-            #     viewer.data.set_colors(colours[current_frame])
-            # else:
-            #     viewer.data.set_vertices(test_data_eigen[current_frame])
-
+            if show_decoded:
+                viewer.data.set_vertices(p2e(base_verts + decoded_pca_displacements[current_frame]))
+                # viewer.data.set_colors(colours[current_frame])
+            else:
+                viewer.data.set_vertices(p2e(base_verts + displacements[current_frame]))
 
             viewer.data.compute_normals()
-            current_frame = (current_frame + 1) % len(displacements_data)
+            current_frame = (current_frame + 1) % len(displacements)
 
         return False
 
