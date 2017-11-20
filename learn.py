306aee55ada411f410ce99c52bb85480c0f3761f
zero-impact <lawsonfulton@gmail.com>
2017-11-20 15:14:46 -0500

    Loading and animating in pyigl working

--- learn.py
+++ learn.py
@@ -1,5 +1,4 @@
 import time
-from multiprocessing import Pool
 
 import numpy
 import scipy
@@ -10,11 +9,63 @@ from iglhelpers import e2p, p2e
 
 import my_utils
 
+current_frame = 0
+
+
 base_path = 'training_data/first_interaction/'
 def main():
-	displacements_data = my_utils.load_displacement_dmats_to_numpy(base_path)
-	print(displacements_data)
+    # Loading the rest pose
+    base_verts, face_indices = my_utils.load_base_vert_and_face_dmat_to_numpy(base_path)
+    base_verts_eig = p2e(base_verts)
+    face_indices_eig = p2e(face_indices)
 
+    # Loading displacements for training data
+    displacements_data = my_utils.load_displacement_dmats_to_numpy(base_path)
+
+
+    
+    # Set up
+    viewer = igl.viewer.Viewer()
+    viewer.data.set_mesh(base_verts_eig, face_indices_eig)
+
+
+    def pre_draw(viewer):
+        global current_frame
+        
+        if viewer.core.is_animating:
+            viewer.data.set_vertices(p2e(base_verts + displacements_data[current_frame]))
+            # if show_decoded:
+            #     viewer.data.set_vertices(test_data_decoded_eigen[current_frame])
+            #     viewer.data.set_colors(colours[current_frame])
+            # else:
+            #     viewer.data.set_vertices(test_data_eigen[current_frame])
+
+
+            viewer.data.compute_normals()
+            current_frame = (current_frame + 1) % len(displacements_data)
+
+        return False
+
+    def key_down(viewer, key, mods):
+        global show_decoded
+        if key == ord(' '):
+            viewer.core.is_animating = not viewer.core.is_animating
+        elif key == ord('D') or key == ord('d'):
+            show_decoded = not show_decoded
+            print(show_decoded)
+
+        return False
+
+
+    viewer.callback_pre_draw = pre_draw
+    viewer.callback_key_down = key_down
+    viewer.core.is_animating = False
+    viewer.core.invert_normals = True
+    viewer.data.face_based = True
+    # viewer.core.camera_zoom = 2.5
+    viewer.core.animation_max_fps = 30.0
+
+    viewer.launch()
 
 if __name__ == "__main__":
-	main()
+    main()
--- my_utils.py
+++ my_utils.py
@@ -1,3 +1,12 @@
+import os
+import re
+import itertools
+from multiprocessing import Pool
+
+import numpy
+
+import pyigl as igl
+from iglhelpers import e2p, p2e
 
 def load_base_vert_and_face_dmat_to_numpy(base_path):
     """ Returns a tuple (verts, faces) """
@@ -11,21 +20,27 @@ def load_base_vert_and_face_dmat_to_numpy(base_path):
 
     return e2p(verts), e2p(faces)
 
+def _read_dmat_helper(args):
+    i, base_path = args
+    filename = base_path + 'displacements_%d.dmat' % i
+    if(i % 13 == 0):
+        print('.', end='', flush=True)
+    displacements = igl.eigen.MatrixXd()
+    igl.readDMAT(filename, displacements)
+    return e2p(displacements)
 
-def load_displacement_dmats_to_numpy(base_path, num_samples):
+def load_displacement_dmats_to_numpy(base_path, num_samples=None):
     """ Returns a numpy array of displacements for num_samples configurations """
+
+    if num_samples is None:  # Compute number of configurations to load automatically
+        filenames = [f for f in os.listdir(base_path) if os.path.isfile(os.path.join(base_path, f))]
+        numbers_strings = [re.findall('\d+', f) for f in filenames]
+        num_samples = max(int(n_str[0]) for n_str in numbers_strings if n_str)
+
     print('Loading', num_samples, 'samples...', end='', flush=True)
     p = Pool(16)
 
-    def read_dmat(i):
-        filename = base_path + 'displacements_%d.dmat' % i
-        if(i % 13 == 0):
-            print('.', end='', flush=True)
-        displacements = igl.eigen.MatrixXd()
-        igl.readDMAT(filename, displacements)
-        return e2p(displacements)
-
-    displacements_samples = numpy.array(p.map(read_dmat, range(num_samples)))
+    displacements_samples = numpy.array(p.map(_read_dmat_helper, zip(range(num_samples), itertools.repeat(base_path))))
     p.terminate()
     print()
     print('Done.')
