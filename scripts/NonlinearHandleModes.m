4ca45bcd0f75e934b7d69341a79248a4ae314055
Vismay <vismay@utexas.edu>
2018-05-28 13:18:22 -0500

    decreased alpha for larger meshes

--- scripts/NonlinearHandleModes.m
+++ scripts/NonlinearHandleModes.m
@@ -98,7 +98,7 @@ while tot_its < num_samples
     rng('shuffle')
     h = zeros(3*nH,1); %handle displacements
     u = zeros(numel(V),1);
-    alpha = 0.1%0.5 %0.1 %step length % set based on size of bounding box?
+    alpha = 0.05%0.5 %0.1 %step length % set based on size of bounding box?
     
     [BB,BF] = bounding_box(V);
     maxLength2 = max(max((BB(:,1) - BB(:,1)').^2+(BB(:,2) - BB(:,2)').^2+(BB(:,3) - BB(:,3)').^2));
