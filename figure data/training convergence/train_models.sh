0410617ecd98472ec3c2500d885a4dd45729de53
zero-impact <lawsonfulton@gmail.com>
2018-10-19 15:53:24 -0400

    Adding submodules and major cleanup

--- figure data/training convergence/train_models.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for filename in model_configs/*.json; do
-    python "/home/lawson/Workspace/AutoDef/scripts/build-model.py" "-f" "True" "-c" "$filename" "-o" "./trained_models_avg_error/$(basename "$filename" .json)"
-done
--- overnight.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-python scripts/unified_gen_and_train.py configs/unified/X.json models/search/X --retrain
-python scripts/unified_gen_and_train.py configs/unified/dragon.json models/search/dragon --retrain
-python scripts/unified_gen_and_train.py configs/unified/armadillo.json models/search/armadillo --retrain
-python scripts/unified_gen_and_train.py configs/unified/catapult.json models/search/catapult --retrain
-python scripts/unified_gen_and_train.py configs/unified/mounted_bar.json models/search/mounted_bar --retrain
